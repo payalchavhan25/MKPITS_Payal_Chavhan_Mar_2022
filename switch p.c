61e875094690551992f1f2f260fea97c3684fb38
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-14 17:48:46 +0530

    Add files via upload

---
+++ switch p.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int choice;
+//printf("enter 1 or 2 or 3");
+//scanf("%d",choice);
+switch(1)
+{
+case 1:
+printf("in case 1");
+break;
+case 2:
+printf("in case 2");
+break;
+case 3:
+printf("in case 3");
+break;
+default :
+printf("i am in default");
+getch();	
+}
+}
