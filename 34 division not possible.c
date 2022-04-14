98b6ff31e10ce1c5514debb4cfb06e3101b8e562
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-14 17:18:30 +0530

    Add files via upload

---
+++ 34 division not possible.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,y;
+	float div_result;
+	printf("input two numbers");
+	printf("x");
+	scanf("%d",&x);
+	printf("y");
+	scanf("%d",&y);
+	 if(y!=0)
+	 {
+	 	div_result=(float)x/(float)y;
+	 	printf("%f\n",div_result);
+	 }
+	 else
+	 {
+	 	printf("divission not possible\n");
+	 }
+	 getch();
+}
