5985355c0c3214d223e36d9d083a3ee6d0dcfe4b
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-14 17:45:18 +0530

    Add files via upload

---
+++ reverse of XML.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char char1 = 'X';
+char char2 = 'M';
+char char3 = 'L';
+printf("the revers of %c%c%c is %c%c%c\n",char1,char2,char3,char3,char2,char1);
+getch();
+}
+
