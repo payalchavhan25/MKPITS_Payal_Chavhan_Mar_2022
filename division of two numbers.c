e04a43be3b0df94c1aae918fae04b7b0bfebf8e1
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-14 17:33:00 +0530

    Add files via upload

---
+++ division of two numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x,y ;
+float div_result;
+printf("input two numbers:");
+scanf("%d%d",&x,&y);
+
+div_result=x/y;
+printf("div_result=%f",div_result);
+getch();
+}
+
