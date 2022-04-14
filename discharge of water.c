cef32503231ebe025e9b2b7201385e4093fad922
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-14 17:29:35 +0530

    Add files via upload

---
+++ discharge of water.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a,v,q;
+printf("enter area and velocity\n");
+scanf("%f%f" ,&a,&v);
+q=a*v;
+printf("discharge of water =%f",q);
+getch();
+}
