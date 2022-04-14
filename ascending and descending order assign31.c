c8dc8cb27a9bdeea05e604cd3432bd3de0509ded
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-14 17:26:21 +0530

    Add files via upload

---
+++ ascending and descending order assign31.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("input two values\n");
+scanf("%d%d",&a,&b);
+if(a!=b)
+{
+if (b>a)
+{
+printf("ascending order\n");
+}
+else
+{
+printf("descending order\n");
+			
+}
+}
+getch();
+	
+}
