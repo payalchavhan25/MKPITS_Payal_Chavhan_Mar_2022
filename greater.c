9af79902ed15c1fd900463c3084c5e46a8d9e9be
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-14 17:37:18 +0530

    Add files via upload

---
+++ greater.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+	
+	int p1,p2,p3;
+	printf("enter p1");
+	scanf("%d", &p1);
+	printf("enter p2");
+	scanf("%d", &p2);
+	printf("enter p3");
+	scanf("%d", &p3);
+	if(p1>p2 && p1>p3);
+	{
+		print("p1 is greater %d", p1);
+		
+	}
+	else
+	if(p2>p1 && p2>p3);
+	{
+		print("p2 is greater %d", p2);
+		
+	}
+	else
+	{
+		print("p3 is greater %d", p3);
+	}
+}	
+	
+	
+
+	
+
+ 
