b5a1e5db62db834a57ffabc71690a32fd7ce5d71
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-25 23:37:31 +0530

    array not present

---
+++ array not present.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,a[5],ser;
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("enter num u want to search");
+	scanf("%d",&ser);
+	for(i=0;i<5;i++)
+	{
+		if(a[i]==ser)
+		{
+	printf("present");
+	}
+	else
+	{
+		printf("not present");
+		break;
+	}
+	printf("\n");
+	}
+	getch();
+}
