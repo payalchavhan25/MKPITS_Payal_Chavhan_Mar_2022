fbe21649c0891bdbe8bf0a40bfda8d19c98359c9
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-26 00:56:50 +0530

    array

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[3];
+	a[0]=56;
+	a[1]=23;
+	a[2]=4;
+	a[3]=12;
+	
+	printf("%d\n",a[0]);
+	printf("%d\n",a[1]);
+	printf("%d\n",a[2]);
+	printf("%d\n",a[3]);
+	
+	getch();
+	
+}
