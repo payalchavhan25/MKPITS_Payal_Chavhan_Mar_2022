a120265dfc21c6f67975ee50d3af0c60bae51bcf
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-28 10:17:16 +0530

    Add files via upload

---
+++ string len.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char name[50]={"dds fvt gbr hyh fttc hn"}; 
+	int len;
+	
+
+    len=strlen(name);
+    printf("%d",len);
+    
+    getch();
+	
+	
+}
