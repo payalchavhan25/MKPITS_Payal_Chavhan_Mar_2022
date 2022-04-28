4dde3e315cb5c5930608efcc924ed391b781084c
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-04-28 10:18:00 +0530

    Add files via upload

---
+++ string rev.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char name[50]={"sachhu"}; 
+	int len;
+	
+	strrev(name);
+	printf("%s",name);
+    
+    getch();
+	
+	
+}
