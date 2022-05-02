2da23fa5d55afdcee2032c306c09e2f759071bec
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-05-02 12:47:52 +0530

    row column 12345,aaaaa.c

---
+++ row column 12345,aaaaa.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char r,c;
+	for(r=1;r<=5;r++)
+	{
+		for(c=1;c<=r;c++)
+		{
+			printf("12345\n");
+			printf("aaaaa\n");
+			
+		}
+		printf("\n");
+	}
+	getch();
+}
