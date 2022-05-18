38f0bf8065450f8df20c822d68163f666cdca324
payalchavhan25 <97234790+payalchavhan25@users.noreply.github.com>
2022-05-18 23:58:25 +0530

    Add files via upload

---
+++ assignment 73.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,num3,res;
+	printf("enter first number to multiply:");
+	scanf("%d",&num1);
+	printf("enter second number to multiply:");
+	scanf("%d",&num2);
+	printf("enter third number to multiply:");
+	scanf("%d",&num3);
+
+	res=num1*num2*num3;
+	printf("%d",res);
+} 
