de90e300546b5d553ffec60be7dce55d9ed0ba0d
payal chavhan <chavhanpayal19@gmail.com>
2022-09-18 22:38:21 +0530

    1 to 10 no using while loop

---
+++ C sharp/1 to 1o number using while loop/1 to 1o number using while loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_to_1o_number_using_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //program to print no from 1 to 10 using while loop
+            int n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+        }
+    }
+}
